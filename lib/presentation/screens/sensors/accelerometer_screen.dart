13607e7c5b0f0322d3f793fa53146ea8b7bc8bf3
LuisPulenta <luisalbertonu@gmail.com>
2023-07-08 10:43:21 -0300

    Video 50

--- lib/presentation/screens/sensors/accelerometer_screen.dart
+++ lib/presentation/screens/sensors/accelerometer_screen.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:miscelaneos/presentation/providers/providers.dart';
 
-class AccelerometerScreen extends StatelessWidget {
-  const AccelerometerScreen({Key? key}) : super(key: key);
+class AccelerometerScreen extends ConsumerWidget {
+  const AccelerometerScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final accelererometer$ = ref.watch(accelerometerGravityProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Acelerómetro'),
-        centerTitle: true,
       ),
-      body: const Center(
-        child: Text('Acelerómetro'),
+      body: Center(
+        child: accelererometer$.when(
+            data: (value) => Text(
+                  value.toString(),
+                  style: const TextStyle(fontSize: 30),
+                ),
+            error: (error, stackTrace) => Text('$error'),
+            loading: () => const CircularProgressIndicator()),
       ),
     );
   }
--- lib/presentation/screens/sensors/gyroscope_screen.dart
+++ lib/presentation/screens/sensors/gyroscope_screen.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:miscelaneos/presentation/providers/providers.dart';
 
-class GyroscopeScreen extends StatelessWidget {
-  const GyroscopeScreen({Key? key}) : super(key: key);
+class GyroscopeScreen extends ConsumerWidget {
+  const GyroscopeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final gyroscope$ = ref.watch(gyroscopeProvider);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Giroscopio'),
-        centerTitle: true,
+        title: const Text('Giroscópio'),
       ),
-      body: const Center(
-        child: Text('Giroscopio'),
+      body: Center(
+        child: gyroscope$.when(
+            data: (value) => Text(
+                  value.toString(),
+                  style: const TextStyle(fontSize: 30),
+                ),
+            error: (error, stackTrace) => Text('$error'),
+            loading: () => const CircularProgressIndicator()),
       ),
     );
   }
