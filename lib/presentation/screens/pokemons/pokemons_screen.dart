f8a6398bc5e09b7ba17f28cb6b1c84c36762df3a
LuisPulenta <luisalbertonu@gmail.com>
2023-07-09 12:43:01 -0300

    Video 67

--- lib/presentation/screens/pokemons/pokemons_screen.dart
+++ lib/presentation/screens/pokemons/pokemons_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:miscelaneos/presentation/providers/providers.dart';
 
 class PokemonsScreen extends StatelessWidget {
   const PokemonsScreen({Key? key}) : super(key: key);
@@ -12,26 +14,83 @@ class PokemonsScreen extends StatelessWidget {
 }
 
 //-------------------------------------------------------------
-class PokemonsView extends StatefulWidget {
+class PokemonsView extends ConsumerStatefulWidget {
   const PokemonsView({super.key});
 
   @override
-  State<PokemonsView> createState() => _PokemonsViewState();
+  PokemonsViewState createState() => PokemonsViewState();
 }
 
-class _PokemonsViewState extends State<PokemonsView> {
-  final scrollCOntroller = ScrollController();
+class PokemonsViewState extends ConsumerState<PokemonsView> {
+//------------------- Variables y métodos --------------------
+  final scrollController = ScrollController();
+
+  void infiniteScroll() {
+    final currentPokemons = ref.read(pokemonIdsProvider);
+    if (currentPokemons.length > 400) {
+      scrollController.removeListener(infiniteScroll);
+      return;
+    }
+    if ((scrollController.position.pixels + 200) >
+        scrollController.position.maxScrollExtent) {
+      ref.read(pokemonIdsProvider.notifier).update((state) => [
+            ...state,
+            ...List.generate(30, (index) => state.length + index + 1)
+          ]);
+    }
+  }
+
+//------------------- initState ------------------------------
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(infiniteScroll);
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  //------------------- Pantalla ------------------------------
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
+      controller: scrollController,
       slivers: [
         SliverAppBar(
           title: const Text('Pokemons'),
           centerTitle: true,
           floating: true,
           backgroundColor: Colors.white.withOpacity(0.8),
-        )
+        ),
+        const _PokemonGrid(),
       ],
+    );
+  }
+}
+
+//-------------------------------------------------------------
+class _PokemonGrid extends ConsumerWidget {
+  const _PokemonGrid();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pokemonIds = ref.watch(pokemonIdsProvider);
+    return SliverGrid.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, crossAxisSpacing: 2, mainAxisSpacing: 2),
+      itemCount: pokemonIds.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () {},
+          child: Image.network(
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png',
+            fit: BoxFit.contain,
+          ),
+        );
+      },
     );
   }
 }
