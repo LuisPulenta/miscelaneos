b80c387f6b0908d7894970e2b22f848a18b68439
LuisPulenta <luisalbertonu@gmail.com>
2023-07-20 16:15:48 -0300

    Video 117

---
+++ lib/presentation/providers/location/map_controller_provider.dart
@@ -0,0 +1,77 @@
+import 'dart:async';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+//----------------------- Provider ----------------------------
+
+final mapControllerProvider =
+    StateNotifierProvider<MapNotifier, MapState>((ref) {
+  return MapNotifier();
+});
+
+//----------------------- Notifier ----------------------------
+
+class MapNotifier extends StateNotifier<MapState> {
+  StreamSubscription? userLocation$;
+
+  MapNotifier() : super(MapState());
+
+  Stream<(double, double)> trackUser() async* {
+    await for (final pos in Geolocator.getPositionStream()) {
+      yield (pos.latitude, pos.longitude);
+    }
+  }
+
+  void setMapController(GoogleMapController controller) {
+    state = state.copyWith(controller: controller, isReady: true);
+  }
+
+  gotoLocation(double latitude, double longitude) {
+    final newPosition = CameraPosition(
+      target: LatLng(latitude, longitude),
+      zoom: 15,
+    );
+    state.controller
+        ?.animateCamera(CameraUpdate.newCameraPosition(newPosition));
+  }
+
+  toggleFollowUser() {
+    state = state.copyWith(followUser: !state.followUser);
+    if (state.followUser) {
+      userLocation$ = trackUser().listen((event) {
+        gotoLocation(event.$1, event.$2);
+      });
+    } else {
+      userLocation$?.cancel();
+    }
+  }
+}
+
+//----------------------- State -------------------------------
+class MapState {
+  final bool isReady;
+  final bool followUser;
+  final List<Marker> markers;
+  final GoogleMapController? controller;
+
+  MapState(
+      {this.isReady = false,
+      this.followUser = false,
+      this.markers = const [],
+      this.controller});
+
+  MapState copyWith({
+    bool? isReady,
+    bool? followUser,
+    List<Marker>? markers,
+    GoogleMapController? controller,
+  }) {
+    return MapState(
+      isReady: isReady ?? this.isReady,
+      followUser: followUser ?? this.followUser,
+      markers: markers ?? this.markers,
+      controller: controller ?? this.controller,
+    );
+  }
+}
--- lib/presentation/screens/location/controlled_map_screen.dart
+++ lib/presentation/screens/location/controlled_map_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:miscelaneos/presentation/providers/providers.dart';
 
 class ControlledMapScreen extends ConsumerWidget {
@@ -7,14 +9,15 @@ class ControlledMapScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final watchUserLocation = ref.watch(watchLocationProvider);
+    //final watchUserLocation = ref.watch(watchLocationProvider);
+    final useInitialLocation = ref.watch(userLocationProvider);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ControlledMapScreen'),
-        centerTitle: true,
-      ),
-      body: watchUserLocation.when(
+      // appBar: AppBar(
+      //   title: const Text('ControlledMapScreen'),
+      //   centerTitle: true,
+      // ),
+      body: useInitialLocation.when(
           data: (data) => _MapAndControls(
                 latitude: data.$1,
                 longitude: data.$2,
@@ -29,13 +32,97 @@ class ControlledMapScreen extends ConsumerWidget {
 
 //-------------------------------------------------------------
 
-class _MapAndControls extends StatelessWidget {
+class _MapAndControls extends ConsumerWidget {
   final double latitude;
   final double longitude;
   const _MapAndControls({required this.latitude, required this.longitude});
 
   @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final mapControllerState = ref.watch(mapControllerProvider);
+
+    return Stack(
+      children: [
+        _MapView(
+          initialLatitude: latitude,
+          initialLongitude: longitude,
+        ),
+        //Regresar a la pantalla anterior
+        Positioned(
+          top: 40,
+          left: 20,
+          child: IconButton.filledTonal(
+              onPressed: () {
+                context.pop();
+              },
+              icon: const Icon(Icons.arrow_back)),
+        ),
+        //Ir a la posición del Usuario
+        Positioned(
+          bottom: 40,
+          left: 20,
+          child: IconButton.filledTonal(
+              onPressed: () {
+                ref
+                    .read(mapControllerProvider.notifier)
+                    .gotoLocation(latitude, longitude);
+              },
+              icon: const Icon(Icons.location_searching)),
+        ),
+        //Seguir Usuario
+        Positioned(
+          bottom: 90,
+          left: 20,
+          child: IconButton.filledTonal(
+            onPressed: () {
+              ref.watch(mapControllerProvider.notifier).toggleFollowUser();
+            },
+            icon: Icon(mapControllerState.followUser
+                ? Icons.directions_run
+                : Icons.accessibility_new_outlined),
+          ),
+        ),
+        //Crear Marcador
+        Positioned(
+          bottom: 140,
+          left: 20,
+          child: IconButton.filledTonal(
+              onPressed: () {
+                context.pop();
+              },
+              icon: const Icon(Icons.pin_drop)),
+        ),
+      ],
+    );
+  }
+}
+
+//------------------------------------------------------------
+
+class _MapView extends ConsumerStatefulWidget {
+  final double initialLatitude;
+  final double initialLongitude;
+  const _MapView(
+      {required this.initialLatitude, required this.initialLongitude});
+
+  @override
+  _MapViewState createState() => _MapViewState();
+}
+
+class _MapViewState extends ConsumerState<_MapView> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return GoogleMap(
+      mapType: MapType.normal,
+      myLocationEnabled: true,
+      zoomControlsEnabled: false,
+      myLocationButtonEnabled: false,
+      initialCameraPosition: CameraPosition(
+          target: LatLng(widget.initialLatitude, widget.initialLongitude),
+          zoom: 12),
+      onMapCreated: (GoogleMapController controller) {
+        ref.read(mapControllerProvider.notifier).setMapController(controller);
+      },
+    );
   }
 }
